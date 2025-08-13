a695d20fd02e7a5b8f4f886799607d687e52a545
Aditya Mishra <108148546+erroradityamishra@users.noreply.github.com>
2025-08-13 16:55:29 +0530

    Add files via upload

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\mishr\OneDrive\Desktop\Code Space✨\School Managemet System\venv\Scripts\python.exe
+from django.core import Information
+
+if __name__ == "__main__":
+    Information.execute_from_command_line()
---
+++ apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class StudentInformationAppConfig(AppConfig):
+    name = 'student_Information_app'
